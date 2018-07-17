76567219096ceeba20ecb1238bac6ab2b0d8d483
victruino <40527382+victruino@users.noreply.github.com>
2018-07-17 00:15:08 -0500

    Add files via upload

---
+++ HC_SR042.ino
@@ -0,0 +1,36 @@
+// declaramos las variables
+int Trig=7;
+int Echo=8;
+int time1;
+int distancia;
+
+
+void setup() {
+// Iniciamos el puerto serial para leer los datos
+//a traves del monitor serial
+Serial.begin(9600);
+// configuramos los pines digitales
+pinMode(Trig,OUTPUT);
+pinMode(Echo,INPUT);
+
+}
+
+void loop() {
+  //Iniciamos llevando el Trig a LOW para asegurarnos que no emita
+  //ningun pulso
+  digitalWrite(Trig,LOW);
+  delayMicroseconds(20);
+  // se envia el pulso
+  digitalWrite(Trig,HIGH);
+  delayMicroseconds(20);
+  // se lleva el Trig nuevamete a LOW para que no emita mas
+  digitalWrite(Trig,LOW);
+  // tomamos el tiempo de la funcion pulseIn
+  time1=pulseIn(Echo,HIGH);
+  //Aplicamos la formula
+  distancia=int((time1*0.034)/2);
+  //Imprimos la distancia en el puerto serial
+  Serial.print("Distancia: ");Serial.println(distancia);
+  delay(1000);
+ }
+
---
+++ HC_SR04_sin_Delay.ino
@@ -0,0 +1,48 @@
+
+int medida;
+int ultra=15;
+int eco=14;
+int dura;
+int distancia;
+int dist;
+bool distOk= false;
+int tomas=0;
+int c1=0;
+unsigned long time1;
+void setup() {
+  // put your setup code here, to run once:
+ 
+Serial.begin(9600);
+pinMode(ultra,OUTPUT);
+pinMode(eco,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(true){
+  time1=micros();
+  if(micros-time1>=10 && c1== 0){
+  digitalWrite(ultra,LOW);
+  c1++;
+  time1=micros();}
+  
+  if(micros-time1>=10 && c1== 1){
+  digitalWrite(ultra,HIGH);
+  c1++;
+  time1=micros();}
+  
+    if(micros-time1>=20 && c1== 2){
+  digitalWrite(ultra,LOW);
+  c1=0;tomas++;
+  dura=pulseIn(eco,HIGH);
+   distancia=int((dura*0.034)/2);
+  if(tomas<=10){dist+=distancia;}
+  if(tomas==10){dist=dist/10; distOk=true;tomas=0;}
+    }
+ if(distOk==true){ 
+ Serial.print("DIS: ");Serial.println(distancia);
+ distOk=false;
+  }
+  }
+ }
